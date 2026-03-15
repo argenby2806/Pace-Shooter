0cd584650f447389cb3f1cc0cc5c84f7688237ca
argenby2806 <doctentoi94@gmail.com>
2026-03-15 21:21:18 +0700

    demo part 7

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -9,4 +9,20 @@ public class PlayerHealth : Health
         base.Die();
         Debug.Log("Player died");
     }
+    public System.Action onDead;
+    public System.Action onHealthChanged;
+
+    public int defaultHealthPoint = 100;
+    public int healthPoint;
+
+    public void TakeDamage(int damage)
+    {
+        if (healthPoint <= 0) return;
+
+        healthPoint -= damage;
+        onHealthChanged?.Invoke();
+
+        if (healthPoint <= 0) Die();
+    }
+
 }
